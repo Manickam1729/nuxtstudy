4e7cde16be11ada38e2c4a22bb489d57b74100d2
Manibarathi29 <manickam@onvi.tech>
2021-03-22 13:28:31 +0000

    tailwind

--- pages/home.vue
+++ pages/home.vue
@@ -1,14 +1,27 @@
 <template>
+
+
+
+<div class="onvi">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
 <section class="section">
+    <div class="head">
  <h1> About ONVI </h1>
+    </div>
+    <div class="mul">
  <p> Indis's first digital multiplex. A unique and innovative platform
  for the movie makers. </p>
+    </div>
+    <div class="movie">
  <p> The movie name is {{ message }} </p>
+ </div>
  <ul>
       <li class="list-group-item"  v-for="movie in movies" :key="movie">{{movie}}</li>
  </ul>
- <button type="submit" @click= "sub()"> Sub </button>
-</section>
+ <button class="bg-red-500 hover:bg-grey-700 text-black font-bold py-2 px-4 rounded" type="submit" @click= "sub()"> Sub </button>
+</section>  
+</div>
 </template>
 
 <script>
@@ -22,11 +35,43 @@ export default {
         return {
            // movieName:'Marra',
             //releaseYear:''
-            movie:''
+            movie:'',
+            message:''
         }
-}
+        },
+        methods: {
+             sub() {
+                 this.message = "Marra";
+                 return this.message;
+                 console.log(this.message);
+             }
+        }
+
 }
 </script>
 
 <style>
+.onvi{
+    display: flex;
+    
+}
+
+.head {
+    background-color:Blue;
+    /*position:relative;
+    left:12rem;
+    */
+    font-family: serif;
+    font-size:40px;
+}
+.mul{
+   display: flex;
+   font-family: sans-serif;
+}
+.movie {
+    display:flex;
+    font-family:monospace;
+        
+
+}
 </style>
